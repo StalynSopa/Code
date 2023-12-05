98360c22d829ce4e62089966cb0021f1521994f5
StalynSopa <131897164+StalynSopa@users.noreply.github.com>
2023-12-05 00:15:14 -0500

    Code
    Este proyecto se basa en el código y los ejemplos que me ayudaran a programar en JavaScript

---
+++ js/saludo.js
@@ -0,0 +1,13 @@
+var nombre = "Kevin";
+var edad = "20";
+var presenteONo = true;
+var apellido = "Sopa";
+var estatura = 1.75;
+
+console.log("Nombre: " + nombre + " " + typeof nombre);
+console.log("Edad: " + edad + " " + typeof edad);
+console.log("Presente: " + presenteONo + " " + typeof presenteONo);
+console.log("Apellido: " + apellido + " " + typeof apellido);
+console.log("Estatura: " + estatura + " " + typeof estatura);
+
+//alert("Hola como estas " + nombre);
